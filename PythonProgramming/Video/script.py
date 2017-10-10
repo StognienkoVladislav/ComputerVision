4c64dfa4d1c0cba2c5317152716cde11b307d722
stognienkovladislav <stognienkovv@gmail.com>
2017-10-10 22:07:44 +0300

    Holly shit

--- PythonProgramming/Video/script.py
+++ PythonProgramming/Video/script.py
@@ -1,9 +1,17 @@
-
-import numpy as np
 import cv2
-
-# Capture video from file
-cap = cv2.VideoCapture(0)
-print(cap.isOpened())
+import numpy as np
+import time
 
 
+cam = cv2.VideoCapture(-1)
+time.sleep(1)
+
+while True:
+    ret,frame = cam.read()
+
+    cv2.imshow('webcam', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
